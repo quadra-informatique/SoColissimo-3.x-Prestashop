f54e7f7ef8575e879aa0e7bd206831c7270c2400
Quadra Team <modules@quadra-informatique.fr>
2014-11-17 14:00:38 +0100

    [*] Increase version to 2.9.10
    Increase version to 2.9.10

--- socolissimo.php
+++ socolissimo.php
@@ -63,7 +63,7 @@ class Socolissimo extends CarrierModule
 	{
 		$this->name = 'socolissimo';
 		$this->tab = 'shipping_logistics';
-		$this->version = '2.9.9';
+		$this->version = '2.9.10';
 		$this->author = 'Quadra Informatique';
 		$this->limited_countries = array('fr');
 		$this->module_key = 'faa857ecf7579947c8eee2d9b3d1fb04';
