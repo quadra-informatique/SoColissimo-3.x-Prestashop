524ec3c2ea451470028887685eccc3056c871456
Quadra Team <modules@quadra-informatique.fr>
2015-08-04 13:56:06 +0200

    [-] MO : bug fix for format belgium phonenumber
    bug fix for format belgium phonenumber

--- controllers/front/redirect.php
+++ controllers/front/redirect.php
@@ -29,10 +29,10 @@ class SocolissimoRedirectModuleFrontController extends ModuleFrontController
 		$inputs = array();
 		foreach ($_GET as $key => $value)
 			if (in_array($key, $fields))
-				$inputs[$key] = Tools::getValue($key);
+				$inputs[$key] = trim(Tools::getValue($key));
 
 		/* for belgium number specific format */
-		if (Tools::getIsset(Tools::getValue('cePays')) && Tools::getValue('cePays') == 'BE')
+		if (Tools::getValue('cePays') == 'BE')
 			if (isset($inputs['cePhoneNumber']) && strpos($inputs['cePhoneNumber'], '324') === 0)
 				$inputs['cePhoneNumber'] = '+324'.Tools::substr($inputs['cePhoneNumber'], 2);
 
